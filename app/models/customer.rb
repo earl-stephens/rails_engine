b51486ef3dcdfc531424ce773257dda51f914737
earl-stephens <stephensearl@hotmail.com>
2019-05-04 17:05:18 -0600

    finished query for customers fave merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,14 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def fave_merchant(id)
+    customer_fave = Merchant.joins(invoices: [:customer, :transactions])
+                            .merge(Transaction.successful)
+                            .where(customers: {id: id})
+                            .select('merchants.*, count(invoices.id) as num_orders')
+                            .group(:id)
+                            .order('num_orders DESC')
+                            .limit(1)
+    customer_fave.first
+  end
 end
